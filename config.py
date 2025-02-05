98acc7c755d0555cd497b2562d95cd23737e5df8
kane5 <Nguyenkhactam5@gmail.com>
2025-02-05 23:13:50 +0700

    Update config.py

--- config.py
+++ config.py
@@ -13,25 +13,25 @@ def is_enabled(value, default):
         return default
       
 # Bot Information
-API_ID = int(environ.get("API_ID", "25811007"))
-API_HASH = environ.get("API_HASH", "72cc7062951f56abb5240994690e49de")
-BOT_TOKEN = environ.get("BOT_TOKEN", "7789014828:AAHCFG1-ZswdY8nJUAC4HAwnUrT3O0GWxjE")
+API_ID = int(environ.get("API_ID", "29762406"))
+API_HASH = environ.get("API_HASH", "146fcf83a1670e2e926ce874ce31fcb3")
+BOT_TOKEN = environ.get("BOT_TOKEN", "7880140653:AAFwU7Zvs5g5QoneNfLWfs5kuuA61Lf20k0")
 
 PICS = (environ.get('PICS', 'https://graph.org/file/ce1723991756e48c35aa1.jpg')).split() # Bot Start Picture
-ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '7240555847').split()]
-BOT_USERNAME = environ.get("BOT_USERNAME", "tymchomapbot") # without @
+ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '6670233911').split()]
+BOT_USERNAME = environ.get("BOT_USERNAME", "KaneSuperBot") # without @
 PORT = environ.get("PORT", "8080")
 
 # Clone Info :-
 CLONE_MODE = bool(environ.get('CLONE_MODE', True)) # Set True or False
 
 # If Clone Mode Is True Then Fill All Required Variable, If False Then Don't Fill.
-CLONE_DB_URI = environ.get("CLONE_DB_URI", "mongodb+srv://nguyenkhactam5:q1231234@cluster0.srj0a.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
-CDB_NAME = environ.get("CDB_NAME", "kanemoji")
+CLONE_DB_URI = environ.get("CLONE_DB_URI", "db:mysql://db_cjz6c4r1ylcj:q1231234@up-de-fra1-mysql-1.db.run-on-seenode.com:11550/db_cjz6c4r1ylcj")
+CDB_NAME = environ.get("CDB_NAME", "db_cjz6c4r1ylcj")
 
 # Database Information
-DB_URI = environ.get("DB_URI", "mongodb+srv://nguyenkhactam5:q1231234@cluster0.srj0a.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
-DB_NAME = environ.get("DB_NAME", "kanemoji")
+DB_URI = environ.get("DB_URI", "db:mysql:// db_cjz6c4r1ylcj:q1231234@up-de-fra1-mysql-1.db.run-on-seenode.com:11550/db_cjz6c4r1ylcj")
+DB_NAME = environ.get("DB_NAME", "db_cjz6c4r1ylcj")
 
 # Auto Delete Information
 AUTO_DELETE_MODE = bool(environ.get('AUTO_DELETE_MODE', True)) # Set True or False
@@ -41,7 +41,7 @@ AUTO_DELETE = int(environ.get("AUTO_DELETE", "30")) # Time in Minutes
 AUTO_DELETE_TIME = int(environ.get("AUTO_DELETE_TIME", "1800")) # Time in Seconds
 
 # Channel Information
-LOG_CHANNEL = int(environ.get("LOG_CHANNEL", ""))
+LOG_CHANNEL = int(environ.get("LOG_CHANNEL", "-1002489615288"))
 
 # File Caption Information
 CUSTOM_FILE_CAPTION = environ.get("CUSTOM_FILE_CAPTION", f"{script.CAPTION}")
@@ -62,7 +62,7 @@ VERIFY_TUTORIAL = environ.get("VERIFY_TUTORIAL", "") # how to open link
 WEBSITE_URL_MODE = bool(environ.get('WEBSITE_URL_MODE', False)) # Set True or False
 
 # If Website Url Mode Is True Then Fill All Required Variable, If False Then Don't Fill.
-WEBSITE_URL = environ.get("WEBSITE_URL", "") # For More Information Check Video On Yt - @Tech_VJ
+WEBSITE_URL = environ.get("WEBSITE_URL", "Https://hethongtop.com") # Subscribe Telegram Channel For - @Hethongkiemlaiatk
 
 # File Stream Config
 STREAM_MODE = bool(environ.get('STREAM_MODE', True)) # Set True or False
@@ -77,8 +77,6 @@ else:
     ON_HEROKU = False
 URL = environ.get("URL", "https://testofvjfilter-1fa60b1b8498.herokuapp.com/")
 
-
-# Don't Remove Credit Tg - @VJ_Botz
-# Subscribe YouTube Channel For Amazing Bot https://youtube.com/@Tech_VJ
-# Ask Doubt on telegram @KingVJ01
+# Subscribe Telegram Channel For - @Hethongkiemlaiatk
+# My Telegram - @Kane_Ai
     
